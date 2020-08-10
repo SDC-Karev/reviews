61805d536e6ca7eb7d15c02fe7c00fa861b56aea
Mike Shertz <shertzm2@gmail.com>
2020-08-10 12:04:38 -0400

    Rendering helpful and recent reviews on screen

---
+++ client/src/HelpfulReviews.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import HelpfulReview from './HelpfulReview.jsx'
+
+class HelpfulReviews extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  // componentDidMount() {
+  // }
+
+  render() {
+    if (!this.props.reviews) {
+      return <span>Loading...</span>
+    }
+    return(
+      <div>
+        <div>Most Helpful Reviews</div>
+        {this.props.reviews.map((review) => {
+          return (
+            <HelpfulReview review={review}/>
+          )
+        })}
+      </div>
+    );
+  }
+}
+
+  export default HelpfulReviews;
---
+++ client/src/RecentReviews.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import RecentReview from './RecentReview.jsx'
+
+class RecentReviews extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  // componentDidMount() {
+  // }
+
+  render() {
+    if (!this.props.reviews) {
+      return <span>Loading...</span>
+    }
+    return(
+      <div className='right-col recent_reviews'>
+        <div>Recently Posted</div>
+        {this.props.reviews.map((review) => {
+          return (
+            <RecentReview review={review}/>
+          )
+        })}
+      </div>
+    );
+  }
+}
+
+  export default RecentReviews;
